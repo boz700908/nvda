42f949f48127cac2cee7cd8589403736fd493d37
Sean Budd <seanbudd123@gmail.com>
2026-04-22 15:49:58 +1000

    Redact secrets when logging config and warn user when changing categories (#19966)
    Closes #19465
    Summary of the issue:
    
    Secrets stored in NVDA config are often unintentionally logged in debug mode by NVDA
    Description of user facing changes:
    
    logging will attempt to redact secrets when the developer decides to sanitise risky log messages.
    Added a new log level: secrets, to disable redactions for required debug logging.
    Added a warning whenever selecting a log level below info.
    Description of developer facing changes:
    
    A new redactSecrets parameter for logging, which searches for and replaces secrets in the log message.
    Description of development approach:
    
    Use Yelp/detect-secrets
    
    This pull request introduces secret redaction support in logging, ensuring that sensitive information is masked in log outputs when requested.
    A new log level is added so you can view unredacted logs if needed.
    
    Secret Redaction in Logging
    
        Added a redactSecrets parameter to the Logger._log method in logHandler.py that, when enabled, uses the detect-secrets library to scan and mask detected secrets in log messages.
        Updated logging calls in source/config/__init__.py to use redactSecrets=True when logging potentially sensitive configuration data.
        Updated the developer documentation to describe the new redactSecrets parameter and recommend its use for sensitive data.
    
    Dependency and Packaging Support
    
        Added detect-secrets as a dependency in pyproject.toml and ensured all relevant submodules are included in frozen builds for dynamic plugin loading
        Include multiprocessing in bundle - needed for import, seems to functionally work?

--- source/config/__init__.py
+++ source/config/__init__.py
@@ -624,13 +624,14 @@ class ConfigManager:
 		except Exception as e:
 			if self._shouldLogConfigAtStartup(profileCopy):
 				# We must log at info level here as the logHandler hasn't been set to log at debug level yet.
-				log.info(f"Config before schema update:\n{profileCopy}")
+				log.info(f"Config before schema update:\n{profileCopy}", redactSecrets=True)
 			raise e
 
 		if self._shouldLogConfigAtStartup(profile):
 			# We must log at info level here as the logHandler hasn't been set to log at debug level yet.
 			log.info(
 				f"Config loaded (after upgrade, and in the state it will be used by NVDA):\n{profile}",
+				redactSecrets=True,
 			)
 		return profile
 
